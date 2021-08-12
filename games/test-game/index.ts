d8cf0c31ab0ee19d3607b6241af990acee493a29
Valentyn Pryvalov <workmail.valentin@gmail.com>
2021-08-12 22:34:05 +0300

    Tanks: added assets

--- games/test-game/index.ts
+++ games/test-game/index.ts
@@ -2,16 +2,17 @@ import {Entry} from "../../core/classes/entry";
 import {Names} from "../../core/global/names";
 import {Configs} from "../../core/lib/services/configs";
 import {LoadingNames} from "../../core/classes/modules/loading_module/static/loading-names";
+import {ILevelData} from "../../core/classes/modules/loading_module/static/loading-interfaces";
 
 class TanksBattleground extends Entry {
-    protected _gameVersion: string = '0.0.3';
+    protected _gameVersion: string = '0.0.4';
 
     protected initGameConfigs(): void {
         super.initGameConfigs();
         const configs: Configs = this.services.get(Names.Services.CONFIGS);
         configs.gameName = 'Tanks Battleground';
         configs.addProperty(LoadingNames.ASSETS, LoadingNames.SCENE, 'scene.json');
-        configs.addProperty(LoadingNames.ASSETS, LoadingNames.LEVELS, ['level.json']);
+        configs.addProperty(LoadingNames.ASSETS, LoadingNames.LEVELS, [{name: 'level_1', path: 'level.json'}] as Array<ILevelData>);
     }
 }
 
